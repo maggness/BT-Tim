91df4063d7208415d73a7a589def62bd4821e4eb
maggness <timderoller@gmail.com>
2022-04-07 13:36:01 +0200

    added support for localstorage

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,38 +1,38 @@
 
 if (window.location.href.indexOf("/resultaten") > -1) {
 } else {
-var form = document.getElementById('formulier'); // form has to have ID: <form id="formulier">
-form.noValidate = true;
-form.addEventListener('submit', function (event) { // listen for form submitting
-  if (!event.target.checkValidity()) {
-      event.preventDefault(); // dismiss the default functionality
-      document.getElementById('validatortekst').innerText = "Je heb nog niet alles ingevuld of je hebt een te hoog nummer ingevoerd, check je antwoorden en probeer het opnieuw."
-  }
-}, false);
+  var form = document.getElementById('formulier'); // form has to have ID: <form id="formulier">
+  form.noValidate = true;
+  form.addEventListener('submit', function (event) { // listen for form submitting
+    if (!event.target.checkValidity()) {
+        event.preventDefault(); // dismiss the default functionality
+        document.getElementById('validatortekst').innerText = "Je heb nog niet alles ingevuld of je hebt een te hoog nummer ingevoerd, check je antwoorden en probeer het opnieuw."
+    }
+  }, false);
 
-if (window.sessionStorage != 'undefined' ) {
-  // input to track
-  var fieldnaam = document.getElementById("formulier");
+if (typeof(Storage) !== "undefined") {
+    // input to track
+    var fieldnaam = document.getElementById("formulier");
 
-  if (sessionStorage.getItem("autosavefieldnaam")) {
+    if (sessionStorage.getItem("autosavefieldnaam")) {
+        // Restore a content of the input
+        fieldnaam.naamstudent.value = sessionStorage.getItem("autosavefieldnaam");
+    } if (sessionStorage.getItem("autosavefieldnummer")) {
       // Restore a content of the input
-      fieldnaam.naamstudent.value = sessionStorage.getItem("autosavefieldnaam");
-  } if (sessionStorage.getItem("autosavefieldnummer")) {
-    // Restore a content of the input
-    fieldnaam.studentnummer.value = sessionStorage.getItem("autosavefieldnummer");
-  }
-
-  // Listen for changes in the input field
-  fieldnaam.addEventListener("change", function () {
-    // save value into sessionStorage object 
-    sessionStorage.setItem("autosavefieldnaam", fieldnaam.naamstudent.value);
-    sessionStorage.setItem("autosavefieldnummer", fieldnaam.studentnummer.value);
-  });
-
-  console.log("Naam: "+sessionStorage.getItem("autosavefieldnaam"))
-  console.log("Nummer: "+sessionStorage.getItem("autosavefieldnummer"))
-} else {
-  document.body.style.background = "yellow"
+      fieldnaam.studentnummer.value = sessionStorage.getItem("autosavefieldnummer");
+    }
+  
+    // Listen for changes in the input field
+    fieldnaam.addEventListener("change", function () {
+      // save value into sessionStorage object 
+      sessionStorage.setItem("autosavefieldnaam", fieldnaam.naamstudent.value);
+      sessionStorage.setItem("autosavefieldnummer", fieldnaam.studentnummer.value);
+    });
+  
+    console.log("Naam: "+sessionStorage.getItem("autosavefieldnaam"))
+    console.log("Nummer: "+sessionStorage.getItem("autosavefieldnummer"))
+  } else {
+  console.log('localStorage is not supported');
 }
 
 
